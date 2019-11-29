1c1ded6898cf6a251702db4caba1c6da0e9628ff
Cero Choe <cero@Ceroui-MacBook-Pro.local>
2019-11-29 17:40:56 +0900

    initial

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react"
+import styles from "./header.module.css"
+
+export default () => (
+  <div className={styles.header}>
+    <div className={styles.name}>Jeongbin Cero Choe</div>
+    <div className={styles.description}>anti-apple</div>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import Header from "../components/header"
+import Container from "../components/container"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Container>
+    <Header />
+    <p>anti-apple</p>
+  </Container>
+)
