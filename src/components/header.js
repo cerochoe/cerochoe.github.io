5dfaae5609594324beae24eaea65275bdf9e8ce1
Cero Choe <cero@Ceroui-MacBook-Pro.local>
2019-11-29 18:39:20 +0900

    Add link

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from "react"
 import styles from "./header.module.css"
+import { Link } from "gatsby"
 
 export default () => (
   <div className={styles.header}>
-    <div className={styles.name}>Jeongbin Cero Choe</div>
+    <Link to="/">
+      <div className={styles.name}>Jeongbin Cero Choe</div>
+    </Link>
     <div className={styles.description}>anti-apple</div>
   </div>
 )
